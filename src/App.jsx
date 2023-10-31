09c91dc75dd6066882929010429a6de54c560334
mayur burange <mayurburange72@gmail.com>
2023-10-31 15:36:38 +0530

    complete update listing functionlity

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import Header from './components/Header'
-import PrivetRoute from './components/PrivetRoute'
-import CreateListing from './pages/createListing'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import PrivetRoute from './components/PrivetRoute';
+import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/updateListing';
 
-const App = () => {
+
+export default function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Header/>
-         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/sign-in" element={<SignIn/>}/>
-            <Route path="/sign-up" element={<SignUp/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route element={<PrivetRoute/>}>
-                  <Route path="/profile" element={<Profile/>}/>
-                  <Route path="/create-listing" element={<CreateListing/>}/>
-            </Route>
-
-         </Routes>
-      
-      </BrowserRouter>
-    </div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route element={<PrivetRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
